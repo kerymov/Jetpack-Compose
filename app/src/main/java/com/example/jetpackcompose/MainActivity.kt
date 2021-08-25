5a2374756f5c7fb2c8a365653deab6bf1e788b06
Ilya Kerymov <kerymov@Ilyas-MacBook-Pro.local>
2021-08-25 15:03:37 +0300

    Just playing with modifiers

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -21,17 +27,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5f)
+                    .padding(top = 50.dp)
+                    .border(10.dp, Brush.horizontalGradient(listOf(Color.White, Color.Cyan)), RectangleShape)
+                    .padding(15.dp)
             ) {
-                Text("Hello")
+                Text("Hello", modifier = Modifier
+                    .border(10.dp, Color.Yellow)
+                    .padding(10.dp)
+                    .clickable {  }
+                )
+                Spacer(modifier = Modifier.height(10.dp))
                 Text("World")
-                Text("!")
             }
         }
     }
