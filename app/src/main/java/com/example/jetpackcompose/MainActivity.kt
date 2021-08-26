a4ab010b9da443693cb93da15052e232178e4e24
Ilya Kerymov <kerymov@Ilyas-MacBook-Pro.local>
2021-08-26 14:12:11 +0300

    Styling Text

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -24,7 +24,16 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,65 +43,49 @@ import com.example.jetpackcompose.ui.theme.Shapes
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.opensans_bold, FontWeight.Bold),
+            Font(R.font.opensans_extrabold, FontWeight.ExtraBold),
+            Font(R.font.opensans_lightitalic, FontWeight.Light),
+            Font(R.font.opensans_regular, FontWeight.Normal),
+            Font(R.font.opensans_semibolditalic, FontWeight.SemiBold),
+        )
+
         setContent {
-            val painter = painterResource(id = R.drawable.landscape)
-            val description = "Landscape on the sunset"
-            val title = "Landscape"
             Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
+                    .fillMaxSize()
+                    .background(Color(0xFF010010))
             ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                fontSize = 50.sp,
+                                color = Color.Magenta
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                fontSize = 50.sp,
+                                color = Color.Magenta
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
                 )
-            }
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
         }
     }
